8ddcf5b17ce5ea149f004f714633c68a99b8fcdf
colino17 <51090962+colino17@users.noreply.github.com>
2024-02-09 02:15:04 -0400

    Update run.sh

--- run.sh
+++ run.sh
@@ -3,7 +3,7 @@ python3 -m wyoming_faster_whisper \
     --uri 'tcp://0.0.0.0:10300' \
     --data-dir /data \
     --download-dir /data "$@" \
-    --model "$MODEL"
-    --beam-size "$BEAM_SIZE"
-    --language "$LANGUAGE"
-    --device "cuda"
+    --model "$MODEL" \
+    --beam-size "$BEAM_SIZE" \
+    --language "$LANGUAGE" \
+    --device cuda
